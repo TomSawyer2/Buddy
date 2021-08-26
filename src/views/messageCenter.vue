a34fdc27bac6a7dce3efd2262adfc0e8abdccd83
TomSawyer2 <6272867@qq.com>
2021-08-26 22:07:24 +0800

    feat:消息中心基本布局确定，路由配置

---
+++ src/views/messageCenter.vue
@@ -0,0 +1,89 @@
+<template>
+  <div
+    style="
+      display: flex;
+      flex-direction: row;
+      width: 100%;
+      justify-content: center;
+      align-items: center;
+    "
+  >
+    <v-container>
+      <v-row dense>
+        <v-col v-for="(item, i) in items" :key="i" cols="12">
+          <v-card color="#FFFFFF">
+            <div class="d-flex flex-no-wrap justify-space-between">
+              <div>
+                <v-card-title
+                  class="text-h5"
+                  v-text="item.title"
+                ></v-card-title>
+
+                <v-card-subtitle v-text="item.artist"></v-card-subtitle>
+
+                <v-card-actions>
+                  <v-btn class="ml-2 mt-5" outlined rounded small> 确定 </v-btn>
+                </v-card-actions>
+              </div>
+
+              <v-avatar class="ma-3" size="125" tile>
+                <v-img :src="item.src"></v-img>
+              </v-avatar>
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        color: "#1F7087",
+        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+        title: "Supermodel",
+        artist: "Foster the People",
+      },
+      {
+        color: "#952175",
+        src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
+        title: "Halcyon Days",
+        artist: "Ellie Goulding",
+      },
+      {
+        color: "#1F7087",
+        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+        title: "Supermodel",
+        artist: "Foster the People",
+      },
+      {
+        color: "#1F7087",
+        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+        title: "Supermodel",
+        artist: "Foster the People",
+      },
+      {
+        color: "#1F7087",
+        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+        title: "Supermodel",
+        artist: "Foster the People",
+      },
+      {
+        color: "#1F7087",
+        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+        title: "Supermodel",
+        artist: "Foster the People",
+      },
+      {
+        color: "#1F7087",
+        src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
+        title: "Supermodel",
+        artist: "Foster the People",
+      },
+    ],
+  }),
+};
+</script>
