d3d3457bff819271d46e0a4c140a27cb7a6ee3a9
TomSawyer2 <6272867@qq.com>
2021-09-03 10:19:09 +0800

    fix:异常参数重置

--- src/config/index.ts
+++ src/config/index.ts
@@ -40,31 +40,32 @@ const ERROR_CODE = {
   TOKEN_GENERATE_ERR: 3, // token生成错误
   TOKEN_NOT_EXISTED: 4, // token不存在
   TOKEN_EXPIRED: 5, // token过期或无效 // TODO 重新登录
-  TOKEN_ERR: 6, // token未知错误
-  TOKEN_FAILED: 7, // token与提交申请的用户不一致
-  PHONENUMBERNOTREGISTERED: 8, // 手机号未注册
-  TOKEN_NOT_EXIST: 9, // token解析出用户不存在
-  USER_EXISTED: 10, // 注册用户名已存在
-  PHONE_EXISTED: 11, // 注册手机号已存在
-  CAPTCHA_GENERATE_ERR: 12, // 验证码生成失败
-  CAPTCHA_NOT_GET: 13, // 注册未生成验证码
-  CAPTCHA_ERR_REG: 14, // 注册验证码错误
+  TOKEN_FAILED: 6, // token与提交申请的用户不一致
+  PHONENUMBERNOTREGISTERED: 7, // 手机号未注册
+  TOKEN_NOT_EXIST: 8, // token解析出用户不存在
+  USER_EXISTED: 9, // 注册用户名已存在
+  PHONE_EXISTED: 10, // 注册手机号已存在
+  CAPTCHA_NOT_GET: 11, // 注册未生成验证码
+  CAPTCHA_ERR_REG: 12, // 注册验证码错误
 
-  UPDATE_INFO_ERR: 15, // 更新个人信息失败
-  TEACHER_NOT_FOUND: 16, // 请求Buddy对象不是老队员
+  TEACHER_NOT_FOUND: 13, // 请求Buddy对象不是老队员
 
-  FIELD_EXISTED: 17, // 新建技术栈已存在
-  FIELD_BLANK: 18, // 技术栈为空
+  FIELD_EXISTED: 14, // 新建技术栈已存在
+  FIELD_BLANK: 15, // 技术栈为空
 
-  APPLY_REASON_BLANK: 19, // 申请理由为空
-  REQUEST_SELF: 20, // 向自己提出buddy申请
-  REQUEST_NOT_FOUND: 21, // 没有找到对应请求
-  QRCODE_ERR: 22, // 没有上传二维码
-  INFO_BLANK: 23, // 没有补全信息
-  REQUEST_MAX_NUM: 24, // 小队员请求数量上限
-  REQ_MAX_NUM: 25,// 老队员已接收了三个小队员
+  APPLY_REASON_BLANK: 16, // 申请理由为空
+  REQUEST_SELF: 17, // 向自己提出buddy申请
+  REQUEST_NOT_FOUND: 18, // 没有找到对应请求
+  QRCODE_ERR: 19, // 没有上传二维码
+  INFO_BLANK: 20, // 没有补全信息
+  REQUEST_MAX_NUM: 21, // 小队员请求数量上限
+  REQ_MAX_NUM: 22,// 老队员已接收了三个小队员
 
-  SEARCH_NOT_FOUND: 28, // 无查询结果
+  SEARCH_NOT_FOUND: 23, // 无查询结果
+
+  INTERNAL_ERR: 24,// 服务器内部错误
+  PARAM_ERR: 25,// 参数错误
+
 };
 
 export { URL, ERROR_CODE };
